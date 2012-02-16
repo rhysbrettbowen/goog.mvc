92f2e34a19c86c66c5160706ad25108a9829eff5
Rhsy <rhysbrettbowen@hotmail.com>
2012-02-16 14:55:33 -0800

    add in collection.js tests and fixes

---
+++ collection_test.js
@@ -0,0 +1,36 @@
+goog.require('mvc.Collection');
+goog.require('mvc.Model');
+
+goog.require('goog.testing.PropertyReplacer');
+goog.require('goog.testing.jsunit');
+
+/** @type {mvc.Model} */
+var model1, model2, model3;
+
+var setUp = function() {
+    model1 = new mvc.Model({'sort':3});
+    model2 = new mvc.Model({'sort':1});
+    model3 = new mvc.Model({'sort':2});
+};
+
+var testUnsortedCollection = function() {
+    var test = new mvc.Collection([model1, model2, model3]);
+    assertEquals('first object should be mock 1', test.at(0), model1);
+    assertEquals('second object should be mock 2', test.at(1), model2);
+    assertEquals('third object should be mock 3', test.at(2), model3);
+};
+
+var testSortedCollection = function() {
+    var sort = function(a, b) {return a.get('sort')-b.get('sort');};
+    var test = new mvc.Collection();
+    test.setComparator(sort);
+    test.add(model1);
+    assertEquals('first object should be mock 1', test.at(0), model1);
+    test.add(model2);
+    assertEquals('first object should be mock 2', test.at(0), model2);
+    assertEquals('second object should be mock 1', test.at(1), model1);
+    test.add(model3);
+    assertEquals('first object should be mock 2', test.at(0), model2);
+    assertEquals('second object should be mock 3', test.at(1), model3);
+    assertEquals('third object should be mock 1', test.at(2), model1);
+};
--- model_test.js
+++ model_test.js
@@ -2,12 +2,23 @@ goog.require('mvc.Model');
 
 goog.require('goog.testing.jsunit');
 
+var simpleModel;
+var emptyModel;
+
+var setUp = function() {
+    simpleModel = new mvc.Model({'a':'exists'});
+    emptyModel = new mvc.Model();
+}
+
 var testSimpleModel = function() {
-    var simpleModel = new mvc.Model({'a':'exists'});
     assertNotNullNorUndefined("New model created", simpleModel);
     assertEquals("Should be able to get 'a'", simpleModel.get('a'), 'exists');
     assertNull("Should return null", simpleModel.get('b'));
     assertEquals("Should be able to change 'a'", simpleModel.set('a', 'changed').get('a'), 'changed')
     assertEquals("Should be able to add new attribute 'b'", simpleModel.set('b', 'new').get('b'), 'new');
-    assertNull("Should be able to remove attribute 'b'", simpleModel.unset('b').get('b'))
+    assertUndefined("Should be able to remove attribute 'b'", simpleModel.unset('b').get('b'))
+};
+
+var testEmptyModel = function() {
+    assertNotNull(emptyModel);
 };
