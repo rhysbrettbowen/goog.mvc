582ed8d222ae20323869b218452b340e502668f2
Rhsy <rhysbrettbowen@hotmail.com>
2012-02-15 18:03:24 -0800

    updates recommended by Jay Young

--- control.js
+++ control.js
@@ -1,14 +1,17 @@
-goog.provide('goog.mvc.control');
+goog.provide('goog.mvc.Control');
 
-goog.require('goog.array');
+goog.require('goog.dom');
+goog.require('goog.object');
 goog.require('goog.ui.Component');
 
 /**
- * @constructor
+ * instantiate with a goog.mvc.Model
  *
+ * @constructor
+ * @param {goog.mvc.Model} model
  * @extends {goog.ui.Component}
  */
-goog.mvc.control = function(model) {
+goog.mvc.Control = function(model) {
     this.setModel(model);
 };
 
@@ -16,21 +19,36 @@ goog.inherits(goog.mvc.control, goog.ui.Component);
 
 // backbone.js views are just like goog.ui.component, this is more of an interface
 
-goog.mvc.control.prototype.remove = function() {
+/**
+ * remove the element and dispose
+ */
+goog.mvc.Control.prototype.remove = function() {
     goog.dom.removeNode(this.getElement());
     this.dispose();
 };
 
-goog.mvc.control.prototype.render = function() {
+goog.mvc.Control.prototype.render = function() {
     goog.base(this, 'render', this.getElement(), true);
     return this;
 };
 
-goog.mvc.control.prototype.delegateEvents = function(events) {
+
+/**
+ * pass an object where the key is "eventType .className" and the value is the
+ * event handler function
+ * example:
+ * var events = {}
+ * goog.object.set(events, goog.events.EventType.CLICK+" ."+goog.getCssName('button'), function(e) {alert("hello");});
+ *
+ * @param {Object.<string, Function>} events
+ */
+goog.mvc.Control.prototype.delegateEvents = function(events) {
     goog.object.forEach(events, function(val, key) {
         goog.events.listen(this.getElement(), key.replace(/\s.*/,''), function(e) {
             if(goog.dom.classes.has(e.target, key.replace(/.*\./,'')))
                 events.val(e);
         }, false, this);
     }, this);
-}
+};
+
+
--- model.js
+++ model.js
@@ -1,6 +1,8 @@
-goog.provide('goog.mvc.model');
+goog.provide('goog.mvc.Model');
 
 goog.require('goog.array');
+goog.require('goog.dom');
+goog.require('goog.events');
 goog.require('goog.events.EventTarget');
 goog.require('goog.object');
 
@@ -8,32 +10,36 @@ goog.require('goog.object');
  * Based on Backbone.js
  */
 
-/**
+/** 
+ * Pass an object with key value pairs for the attributes of the model
+ *
+ * @constructor
  * @extends goog.events.EventTarget
  * @param {Object} attr
  */
-goog.mvc.model = function(attr) {
-    /** @private */
-    this.attr_ = [];
+goog.mvc.Model = function(attr) {
+    /**
+    * @private
+    * @type {Object.<string, {val: string, prev: ?string}>}
+    */
+    this.attr_ = {};
     goog.object.forEach(attr, function(val, name) {
-        this.attr_.push({name: name, val: val});
+        this.attr_[name] = {val: val, prev: null};
     }, this);
     this.init();
-}
+};
+
+goog.inherits(goog.mvc.Model, goog.events.EventTarget);
 
 /* this should be overriden for initialisation */
-goog.mvc.model.prototype.init = goog.nullFunction;
+goog.mvc.model.prototype.init = goog.abstractMethod;
 
 /**
  * @param {string} key
+ * @return {string=}
  */
 goog.mvc.model.prototype.get = function(key) {
-    var ret = goog.array.find(this.attr_, function(attr) {
-        return attr.name == key;
-    });
-    if ret
-        return ret.value;
-    return null;
+    return goog.object.get(this.attr_, key, null);
 }
 
 /**
@@ -41,68 +47,75 @@ goog.mvc.model.prototype.get = function(key) {
  * @return {boolean}
  */
 goog.mvc.model.prototype.has = function(key) {
-    return !!goog.array.find(this.attr_, function(attr) {
-        return attr.name == key;
-    });
+    return goog.object.containsKey(this.attr_, key);
 };
 
 /**
- * @param {Object|string} key
- * @param {string=} val
- * @param {Object=} opt
+ * set either a map of key values or a key value
+ *
+ * @param {Object|string} key object of key value pairs to set, or the key
+ * @param {string=} val to use if the key is a string
+ * @param {boolean=} silent true if no change event should be fired
+ * @return {goog.mvc.model}
  */
-goog.mvc.model.prototype.set = function(key, val, opt) {
+goog.mvc.model.prototype.set = function(key, val, silent) {
     if(goog.isObject(key)) {
         goog.object.forEach(key, function(val, key) {
             this.set(key, val, opt);
         }, this);
     }
-    attr = goog.array.find(this.attr_, function(attr) {
-        return attr.name == key;
-    })
+    attr = goog.object.get(this.attr_, attr, null);
     if(!attr) {
-        this.attr_.push({name: name, val: val});
-        attr = this.attr_[this.attr_.length - 1];
+        attr = {val: val, prev: null};
+        goog.object.set(this.attr_, key, attr);
     } else {
         if attr.val == val;
             return this;
         attr.prev = attr.val;
         attr.val = val;
     }
-    if(!opt)
-        this.dispatchEvent(goog.events.EventType.CHANGE, attr);
+    if(!silent) {
+        var ret = {};
+        goog.object.set(ret, key, attr);
+        this.dispatchEvent(goog.events.EventType.CHANGE, ret);
+    }
     return this;
 };
 
+/**
+ * @param {string} key
+ * @return {boolean} if the key existed and has been unset
+ */
 goog.mvc.model.prototype.unset = function(key) {
-    return goog.array.removeIf(this.attr_, function(attr) {
-        return attr.name == key;
-    });
+    return goog.object.remove(this.attr_, key);
 };
 
+/**
+ * fires the change event for all attributes
+ */
 goog.mvc.model.prototype.change = function() {
     this.dispatchEvent(goog.events.EventType.change, this.attr_.slice());
 };
-    
+
+/**
+ * returns the previous value of the attribute
+ *
+ * @param {string} key
+ * @return {?string}
+ */
 goog.mvc.model.prototype.prev = function(key) {
-    var ret = goog.array.find(this.attr_, function(attr) {
-        return attr.name == key;
-    });
-    if ret
-        return ret.prev;
-    return null;
+    return goog.object.get(this.attr_, key, null);
 }
 
-// backbone.js uses bind
-goog.mvc.model.prototype.change = function(name, el, fn) {
-    var model = this.getModel();
-    goog.events.listen(model, function(e) {
-        target = goog.array.find(e.target, model.get(name));
+// binds a change event
+goog.mvc.model.prototype.bind = function(name, el, fn) {
+    goog.events.listen(this, function(e) {
+        target = goog.object.get(e.target, name);
         if(target) {
             if(goog.isFunction(fn)) {
                 fn(el, target.val);
             } else {
-                el.innerHTML = target.val;
+                goog.dom.setTextContent(target.val);
             }
         }
     })
